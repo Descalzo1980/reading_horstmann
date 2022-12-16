c72abd65441a203922b2e0d804656a04c12af246
ragioniere1980@gmail.com <ragioniere1980@gmail.com>
2022-12-16 23:23:51 +0300

    done and viva Java

--- src/chapter_three/Constant2.java
+++ src/chapter_three/Constant2.java
@@ -1,9 +1,16 @@
 package chapter_three;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+import java.util.Date;
+import java.util.Scanner;
+
 public class Constant2 {
 
     public static final double CM_PER_INCH = 2.54;
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         enum Size {Small,Medium,Large,Big}
         Size size = Size.Big;
         String res = size.toString().toUpperCase();
@@ -27,7 +34,63 @@ public class Constant2 {
         int nx = (int)b;
         System.out.println(nx);
         String greeting = "Hello";
-        String s = greeting.substring(0,3);
+        String s = greeting.substring(0,3) + "p!";
         System.out.println(s);
+        String all = String.join("/","H","E","L","L","O");
+        System.out.println(all);
+        String repeated = "Viva Java ".repeat(3);
+        System.out.println(repeated);
+        boolean t = "Hello".equalsIgnoreCase(greeting);
+        boolean r = "Hello".equals(greeting);
+        System.out.println(t);
+        System.out.println(r);
+        String greetingNew = "Hello";
+        int newNum = greetingNew.length();
+        System.out.println(newNum);
+        int countGreeting = greetingNew.codePointCount(0,greetingNew.length());
+        System.out.println(countGreeting);
+        char first = greetingNew.charAt(0);
+        System.out.println(first);
+        int index = greetingNew.offsetByCodePoints(0,3);
+        System.out.println(index);
+        String qw = "Hello             ";
+        String qw1 = "Hello";
+        System.out.println(qw.equalsIgnoreCase(qw1));
+        System.out.println(qw.startsWith(qw1));
+        System.out.println(qw.endsWith(qw1));
+        System.out.println(qw.replace("lo", "p"));
+        System.out.println(qw.toLowerCase());
+        System.out.println(qw.toUpperCase());
+        System.out.println(qw.trim());
+        System.out.println(qw.trim().repeat(2));
+
+        StringBuilder builder = new StringBuilder();
+        builder.append("H");
+        builder.append("e");
+        builder.append("l");
+        builder.append("p");
+        builder.append("!");
+        String completedString = builder.toString();
+        System.out.println(completedString);
+        builder.insert(5,"?");
+        System.out.println(builder);
+
+/*        Scanner in = new Scanner(System.in);
+        System.out.println("What is your name?");
+        String name = in.nextLine();
+        System.out.println("What is your age?");
+        int age = in.nextInt();
+        System.out.println("Hello " + name + " your age " + age);*/
+
+        double xf = 10000 / 3.0;
+        System.out.printf("%8.2f",xf);
+        System.out.printf("%tc",new Date());
+        System.out.println("*".repeat(20));
+        File file = new File("D:\\myfile.txt");
+        Scanner sc =new Scanner(file,StandardCharsets.UTF_8);
+        while(sc.hasNextLine()){
+            System.out.println(sc.nextLine());
+        }
+
     }
 }
